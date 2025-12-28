1261a46fabb0b353be1f52ff77b0245aa9c170f4
kobalicek <kobalicek.petr@gmail.com>
2025-12-28 16:48:45 +0100

    Fixed including AsmJit in CMake

--- tools/configure-makefiles.sh
+++ tools/configure-makefiles.sh
@@ -8,10 +8,10 @@ if [ -n "${ASMJIT_DIR}" ]; then
   BUILD_OPTIONS="${BUILD_OPTIONS} -DASMJIT_DIR=\"${ASMJIT_DIR}\""
 fi
 
-echo "== [Configuring Build - Debug] =="
+echo "== [configure debug] =="
 eval cmake "${CURRENT_DIR}/.." -B "${BUILD_DIR}/Debug" -DCMAKE_BUILD_TYPE=Debug ${BUILD_OPTIONS}
 echo ""
 
-echo "== [Configuring Build - Release] =="
+echo "== [configure release] =="
 eval cmake "${CURRENT_DIR}/.." -B "${BUILD_DIR}/Release" -DCMAKE_BUILD_TYPE=Release ${BUILD_OPTIONS}
 echo ""
